0788f47f9da2f4803b8ae48781020ca07439aac5
Jerem <alguna62@gmail.com>
2022-05-12 12:15:11 +0200

    Modify LICENSE.txt

--- Arti_finder/arti_finder.ps1
+++ Arti_finder/arti_finder.ps1
@@ -1,8 +1,11 @@
 # It allows you to investigate a Windows machine and look for possible artifacts that may have been left behind after its infection.
 
 # Copyright (C) 2022 Alguna, Cyberzen (https://www.cyberzen.com/)
+
 # This program is free software; you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation; either version 3 of the License, or (at your option) any later version.
+
 # This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+
 # You should have received a copy of the GNU General Public License along with this program; if not, see <https://www.gnu.org/licenses>.
 
 $ascii_art = @"
